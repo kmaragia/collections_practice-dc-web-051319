6f4acf7fe89edc8e0711bbe92eb80d3ae722d03c
Kemunto Maragia <kmaragia@email.wm.edu>
2020-10-03 00:02:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(arr)
+    arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort
+end
+
+def swap_elements(arr)
+end 
+
+
+    
+  
